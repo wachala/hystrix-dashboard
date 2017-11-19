935c73eee851348f2cb3dab14f3a4ea86d83532e
Piotr Wachala <piotr.wachala@outlook.com>
2017-11-19 14:52:37 +0100

    Add support for configuration service

--- src/main/java/hystrix/dashboard/HystrixDashboardApplication.java
+++ src/main/java/hystrix/dashboard/HystrixDashboardApplication.java
@@ -2,9 +2,11 @@ package hystrix.dashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @EnableHystrixDashboard
+@EnableEurekaClient
 @SpringBootApplication
 public class HystrixDashboardApplication {
 
